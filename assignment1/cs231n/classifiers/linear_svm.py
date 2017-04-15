0013e0db021b6b1af068555846e2171953f15acf
sjain-stanford <sambhav.eee@gmail.com>
2017-04-15 15:10:43 +0000

    svm update

--- assignment1/cs231n/classifiers/linear_svm.py
+++ assignment1/cs231n/classifiers/linear_svm.py
@@ -34,14 +34,21 @@ def svm_loss_naive(W, X, y, reg):
         continue
       margin = scores[j] - correct_class_score + 1 # note delta = 1
       if margin > 0:
-        loss += margin
+        loss += margin        
+        # Gradient (see http://cs231n.github.io/optimization-1/#analytic)
+        # w.r.t. weights of incorrect class
+        dW[:,j] += X[i]
+        # w.r.t. weights of correct class
+        dW[:,y[i]] -= X[i]
 
   # Right now the loss is a sum over all training examples, but we want it
   # to be an average instead so we divide by num_train.
   loss /= num_train
+  dW /= num_train
 
   # Add regularization to the loss.
   loss += reg * np.sum(W * W)
+  dW += 2*reg*W
 
   #############################################################################
   # TODO:                                                                     #
@@ -51,7 +58,7 @@ def svm_loss_naive(W, X, y, reg):
   # loss is being computed. As a result you may need to modify some of the    #
   # code above to compute the gradient.                                       #
   #############################################################################
-
+  
 
   return loss, dW
 
@@ -62,6 +69,7 @@ def svm_loss_vectorized(W, X, y, reg):
 
   Inputs and outputs are the same as svm_loss_naive.
   """
+  num_train = X.shape[0]
   loss = 0.0
   dW = np.zeros(W.shape) # initialize the gradient as zero
 
@@ -70,7 +78,14 @@ def svm_loss_vectorized(W, X, y, reg):
   # Implement a vectorized version of the structured SVM loss, storing the    #
   # result in loss.                                                           #
   #############################################################################
-  pass
+  scores = X.dot(W)   # (N, C)
+  correct_class_scores = scores[np.arange(num_train), y].reshape(num_train, -1)   # (N, 1)
+  margins = scores - correct_class_scores + 1    # (N, C)
+  margins[np.arange(num_train), y] = 0
+  hinge_loss = margins * (margins > 0)    # (N, C)
+  data_loss = np.sum(hinge_loss) / num_train    # ()
+  reg_loss = reg * np.sum(W * W)
+  loss = data_loss + reg_loss
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
